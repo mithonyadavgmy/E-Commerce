f9731dce150b0cb2d921403a3b489004e4815f2a
mithonyadavgmy <mithonyadavgmy@gmail.com>
2023-10-16 09:18:22 -0700

    success

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+app.use(express.static(path.join(__dirname,'/dist/ecommerce-store')));
+
+app.use('*', function(req, res) {
+    res.sendFile(path.join(__dirname,'/dist/ecommerce-store/index.html'));
+});
+
+app.listen(2121, () => {
+    // console.log("Node serve is running");
+});
